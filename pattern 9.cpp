0e0ded3f61449f194dd4fb035f128e29a15eb89e
sahilbhatt89 <77490766+sahilbhatt89@users.noreply.github.com>
2021-02-24 19:48:24 +0530

    Add files via upload

---
+++ pattern 9.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+    for(int i=n;i>=1;i--)
+    {
+    	for(int j=i;j<n;j++)
+    	{
+    		cout<<" ";
+		}
+		for(int k=1;k<=(2*i-1);k++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
